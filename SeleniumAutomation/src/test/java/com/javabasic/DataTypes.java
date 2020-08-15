0ad7b2c0841498333890838e4c701370af7b3934
JairCk08 <69393808+JairCk08@users.noreply.github.com>
2020-08-15 12:01:42 -0500

    Changes

---
+++ SeleniumAutomation/src/test/java/com/javabasic/DataTypes.java
@@ -0,0 +1,51 @@
+package com.javabasic;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		
+//		byte x;
+//		short y;
+//		int z;
+//		long a;
+//		
+//		float f;
+//	
+//		x = 127;
+//		y = 32767;
+//		z = 2147483647;
+//		a = 9223372036854775807L;
+//		
+//		System.out.println(x);
+		
+		/*DECIMALES*/
+		
+//		float base;
+//		double altura;
+//		
+//		base = 30.5f;
+//		altura = 40.78;
+//		
+//		System.out.println(base);
+		
+		/*CARACTERES*/
+		
+//		char letra1;
+//		char letra2;
+//		String frase;
+//		
+//		letra1 ='a';
+//		letra2= 'a';
+//		frase = "hola";
+//		
+//		System.out.println(letra2);
+//	
+		
+		boolean avesVuelan;
+		avesVuelan = true;
+		System.out.println("Las aves vuelan " + avesVuelan);
+		
+	}
+
+}
---
+++ SeleniumAutomation/src/test/java/com/javabasic/OperatorStatements.java
@@ -0,0 +1,40 @@
+package com.javabasic;
+
+public class OperatorStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*Operadores aritmeticos*/
+//		int x = 1;
+//		int y = 2;
+//		int suma = x * y;
+//		
+//		System.out.println("El resultado de la suma es: "+ suma);
+		
+		/*
+		 * operadores unarios
+		 */
+		
+//		int valor1 = 3;
+//		int incrementar = --valor1;
+//		
+//		System.out.println(incrementar);
+		
+		
+		/*
+		 * Operator racionale
+		 */
+		
+		int num1 = 1;
+		int num2 = 6;
+		if (num1>=10||num2<5) {
+			System.out.println("Cumple");
+		} else {
+			System.out.println("No cumple");
+		}
+		
+		
+	}
+
+}
